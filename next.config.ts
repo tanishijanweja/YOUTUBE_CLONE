79b29ca658741d7efac2fb3936e5448e2d51f8f7
Tanishi <tanishi.janweja@gmail.com>
2025-04-18 23:50:53 +0530

    New commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
 
-export default nextConfig;
+// export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    allowedDevOrigins: [
+      'https://8ba9-2405-201-400d-d16f-986b-c147-2475-12b5.ngrok-free.app',
+    ],
+  },
+}
+
+module.exports = nextConfig
+module.exports = {
+  experimental: {
+    allowedDevOrigins: ['http://complete-mastodon-typically.ngrok-free.app'],
+  },
+};

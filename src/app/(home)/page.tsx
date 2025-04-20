021216bcd06b0014c86e92bc8b381cd4498c85c9
Tanishi <tanishi.janweja@gmail.com>
2025-04-20 23:18:35 +0530

    trpc addons commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,9 @@
+"use-client";
+
+import { trpc } from "@/trpc/client";
+
 export default function Home() {
-  return (
-    <div>
-      I will load videos in future
-    </div>
-  );
+  console.log("trpc object: ", trpc);
+  const { data } = trpc.hello.useQuery({ text: "Tanishi" });
+  return <div>Client component says: {data?.greeting}</div>;
 }
---
+++ src/trpc/client.tsx
@@ -0,0 +1,50 @@
+'use client';
+import type { QueryClient } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { httpBatchLink } from "@trpc/client";
+import { createTRPCReact } from "@trpc/react-query";
+import { useState } from "react";
+import { makeQueryClient } from "./query-client";
+import type { AppRouter } from "./routers/_app";
+
+export const trpc = createTRPCReact<AppRouter>();
+console.log("TRPC client initialized: ", trpc);
+let clientQueryClientSingleton: QueryClient;
+function getQueryClient() {
+  if (typeof window === "undefined") {
+    return makeQueryClient();
+  }
+  return (clientQueryClientSingleton ??= makeQueryClient());
+}
+
+function getUrl() {
+  const base = (() => {
+    if (typeof window !== "undefined") return "";
+    if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+    return "http://localhost:3000";
+  })();
+  return `${base}/api/trpc`;
+}
+export function TRPCProvider(
+  props: Readonly<{
+    children: React.ReactNode;
+  }>
+) {
+  const QueryClient = getQueryClient();
+  const [trpcClient] = useState(() =>
+    trpc.createClient({
+      links: [
+        httpBatchLink({
+          url: getUrl(),
+        }),
+      ],
+    })
+  );
+  return (
+    <trpc.Provider client={trpcClient} queryClient={QueryClient}>
+      <QueryClientProvider client={QueryClient}>
+        {props.children}
+      </QueryClientProvider>
+    </trpc.Provider>
+  );
+}

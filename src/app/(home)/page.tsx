23c8c8a126a72c02d0bacb7f69425cec1ad0b58e
Tanishi <tanishi.janweja@gmail.com>
2025-05-01 15:56:33 +0530

    New commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,12 +1,18 @@
-"use client";
+import { HydrateClient, trpc } from "@/trpc/server";
+import { PageClient } from "./client";
+import { Suspense } from "react";
+// import { ErrorBoundary } from "next/dist/client/components/error-boundary";
+import { ErrorBoundary } from "react-error-boundary";
 
-import { trpc } from "@/trpc/client";
-
-export default function Home() {
-  const { data } = trpc.hello.useQuery({ text: "Tanishi" });
-  return(
-     <div>
-      Client component says: { data?.greeting }
-      </div>
-    )
+export default async function Home() {
+  void trpc.hello.prefetch({ text: "Tanishi" });
+  return (
+    <HydrateClient>
+      <Suspense fallback={<p>Loading...</p>}>
+        <ErrorBoundary fallback={<p>Error...</p>}>
+          <PageClient />
+        </ErrorBoundary>
+      </Suspense>
+    </HydrateClient>
+  );
 }
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,11 @@
-import { z } from 'zod';
-import { baseProcedure, createTRPCRouter } from '../init';
+import { z } from "zod";
+import { baseProcedure, createTRPCRouter } from "../init";
 export const appRouter = createTRPCRouter({
   hello: baseProcedure
     .input(
       z.object({
         text: z.string(),
-      }),
+      })
     )
     .query((opts) => {
       return {

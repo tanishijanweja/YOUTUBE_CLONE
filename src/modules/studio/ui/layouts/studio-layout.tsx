dcc19c32946301e23fe9ac2858d3afd514bb3209
Tanishi <tanishi.janweja@gmail.com>
2025-05-06 00:47:36 +0530

    Studio setup

--- src/modules/studio/ui/layouts/studio-layout.tsx
+++ src/modules/studio/ui/layouts/studio-layout.tsx
@@ -1,7 +1,7 @@
 import { SidebarProvider } from "@/components/ui/sidebar";
 
-import { HomeNavbar } from "../components/home-navbar";
-import { HomeSidebar } from "../components/home-sidebar";
+import { StudioNavbar } from "../components/studio-navbar";
+import { StudioSidebar } from "../components/studio-sidebar";
 interface StudioLayoutProps {
   children: React.ReactNode;
 }
@@ -10,9 +10,9 @@ export const StudioLayout = ({ children }: StudioLayoutProps) => {
   return (
     <SidebarProvider>
       <div className="w-full">
-        <HomeNavbar />
+        <StudioNavbar />
         <div className="flex min-h-screen pt-[4rem]">
-          <HomeSidebar />
+          <StudioSidebar />
           <main className="flex-1 overflow-y-auto">{children}</main>
         </div>
       </div>

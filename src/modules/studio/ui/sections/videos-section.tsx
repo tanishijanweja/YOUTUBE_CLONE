b566608dfe4d255106e0ee86be4c5683cc86eb8a
Tanishi <tanishi.janweja@gmail.com>
2025-05-08 18:04:40 +0530

    Mux integration

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -13,6 +13,7 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
+import Link from "next/link";
 
 export const VideosSection = () => {
   return (
@@ -25,7 +26,7 @@ export const VideosSection = () => {
 };
 
 export const VideosSectionSuspense = () => {
-  const [data, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
+  const [videos, query] = trpc.studio.getMany.useSuspenseInfiniteQuery(
     {
       limit: DEFAULT_LIMIT,
     },
@@ -40,11 +41,38 @@ export const VideosSectionSuspense = () => {
           <TableHeader>
             <TableRow>
               <TableHead className="pl-6 w-[510px]">Video</TableHead>
+              <TableHead>Visiblity</TableHead>
+              <TableHead>Status</TableHead>
+              <TableHead>Date</TableHead>
+              <TableHead className="text-right">Views</TableHead>
+              <TableHead className="text-right">Comments</TableHead>
+              <TableHead className="text-right pr-6">Likes</TableHead>
             </TableRow>
           </TableHeader>
+          <TableBody>
+            {videos.pages
+              .flatMap((page) => page.items)
+              .map((video) => (
+                <Link
+                  href={`/studio/videos/${video.id}`}
+                  key={video.id}
+                  legacyBehavior
+                >
+                  <TableRow className="cursor-pointer">
+                    <TableCell>{video.title}</TableCell>
+                    <TableCell>visiblity</TableCell>
+                    <TableCell>status</TableCell>
+                    <TableCell>date</TableCell>
+                    <TableCell>views</TableCell>
+                    <TableCell>comments</TableCell>
+                    <TableCell>likes</TableCell>
+                  </TableRow>
+                </Link>
+              ))}
+          </TableBody>
         </Table>
       </div>
-      {JSON.stringify(data)}
+
       <Infinitescroll
         isManual
         hasNextPage={query.hasNextPage}

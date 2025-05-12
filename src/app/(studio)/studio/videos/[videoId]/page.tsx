b19f8c6a50b644ee2739f237abfa2f5229d0fdec
Tanishi <tanishi.janweja@gmail.com>
2025-05-12 23:54:45 +0530

    Video form updated

--- src/app/(studio)/studio/videos/[videoId]/page.tsx
+++ src/app/(studio)/studio/videos/[videoId]/page.tsx
@@ -9,6 +9,7 @@ interface PageProps {
 const Page = async ({ params }: PageProps) => {
   const { videoId } = await params;
   void trpc.studio.getOne.prefetch({ id: videoId });
+  void trpc.categories.getMany.prefetch();
 
   return (
     <HydrateClient>
